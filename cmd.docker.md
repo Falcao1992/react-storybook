9c3b449a808932545a305af269ebab1e04d897c8
Falcao1992 <eduardo.marselle@gmail.com>
2021-11-17 22:50:19 +0100

    add redux and convert TaskList to PureTaskList

---
+++ cmd.docker.md
@@ -0,0 +1,8 @@
+## Creer image
+```docker build -t story-image .```
+
+## Lance l'image dans un container
+```docker run --rm -p 3000:3000 story-image```
+
+```docker-compose up -d --build```
+
--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Eduardo est une salope
+          learn react
         </a>
       </header>
     </div>
